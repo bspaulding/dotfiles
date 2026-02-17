f78eaf320d9e7eabd8a12f5bf8de1dc608527bbb
Bradley Spaulding <brad@tokaidohealth.com>
2026-02-17 11:26:10 -0800

    fixed open-project.fish

--- fish/functions/open-project.fish
+++ fish/functions/open-project.fish
@@ -1,8 +1,7 @@
 function open-project
   set -l wd $argv[1]
   set -l session_name (string replace -a "." "_" (basename $wd))
-  tmux new -s $session_name -c $wd -d
-  tmux send-keys -t $session_name vim Space . Enter
+  tmux new -s $session_name -c $wd -d vim .
   tmux new-window -t $session_name -c $wd
   tmux switchc -t $session_name
 end
