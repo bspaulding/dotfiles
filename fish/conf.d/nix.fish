fbed894543a77ad02f1f46fb438628a347bda655
Bradley Spaulding <brad.spaulding@gmail.com>
2026-04-21 13:41:07 -0700

    added nix default profile to path if missing

--- fish/conf.d/nix.fish
+++ fish/conf.d/nix.fish
@@ -1,3 +1,7 @@
+if test -d /nix/var/nix/profiles/default/bin
+    fish_add_path /nix/var/nix/profiles/default/bin
+end
+
 if test -d $HOME/.nix-profile/bin
     fish_add_path $HOME/.nix-profile/bin
 end
