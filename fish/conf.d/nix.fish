60ead77cbbafecb43f53d5b87a52c45a6f9991fd
Bradley Spaulding <brad@tokaidohealth.com>
2026-08-27 11:07:55 -0700

    added conditional path add for nix binary

--- fish/conf.d/nix.fish
+++ fish/conf.d/nix.fish
@@ -5,3 +5,7 @@ end
 if test -d $HOME/.nix-profile/bin
     fish_add_path $HOME/.nix-profile/bin
 end
+
+if test -d /nix/var/nix/profiles/default/bin
+    fish_add_path /nix/var/nix/profiles/default/bin
+end
